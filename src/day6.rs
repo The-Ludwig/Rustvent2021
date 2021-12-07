afcf6b6fb95bfbc1ae49ee99ecd2955547811b68
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2021-12-07 19:17:54 +0100

    :santa: solved day6 in efficient way :)

--- src/day6.rs
+++ src/day6.rs
@@ -1,50 +1,40 @@
 use std::fs;
 
-fn parse(filename: &str) -> std::io::Result<Vec<u8>> {
-    Ok(fs::read_to_string(filename)?
+fn parse(filename: &str) -> std::io::Result<Vec<usize>> {
+    let mut states = vec![0; 9];
+    for i in fs::read_to_string(filename)?
         .split(",")
-        .map(|x| x.parse().unwrap())
-        .collect())
-}
-
-fn step_loop(fish: &mut Vec<u8>) {
-    let mut n_to_add = 0;
-
-    for i in fish.iter_mut() {
-        if *i > 0 {
-            *i -= 1
-        } else {
-            *i = 6;
-            n_to_add += 1;
-        }
+        .map(|x| x.parse::<usize>().unwrap())
+    {
+        states[i] += 1;
     }
 
-    for _ in 0..n_to_add {
-        fish.push(8);
-    }
+    Ok(states)
 }
 
-fn n_step(fish: &mut Vec<u8>, n: u32) {
+fn step(fish: &mut Vec<usize>) {
+    let mut save = fish[0];
+    for i in (0..9).rev() {
+        let other_save = fish[i];
+        fish[i] = save;
+        save = other_save;
+    }
+    fish[6] += save;
+}
+
+fn n_step(fish: &mut Vec<usize>, n: u32) {
     for _ in 0..n {
-        step_loop(fish);
-    }
-}
-
-/// At least efficient, if we only have 8 numbers ;)
-fn n_step_efficient(fish: &mut Vec<u8>, n: u32) {
-    let mut nums = vec![0u32; 9];
-    for f in fish {
-        nums[*f as usize] += 1;
+        step(fish);
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let mut fish = parse("inputs/day6")?;
     n_step(&mut fish, 80);
-    println!("Answer part 1: {}", fish.len());
+    println!("Answer part 1: {}", fish.iter().sum::<usize>());
 
-    n_step(&mut fish, 10);
-    println!("Answer part 2: {}", fish.len());
+    n_step(&mut fish, 256 - 80);
+    println!("Answer part 2: {}", fish.iter().sum::<usize>());
 
     Ok(())
 }
@@ -56,32 +46,29 @@ mod tests {
     #[test]
     fn test_input() {
         let input = parse("inputs/day6_test").unwrap();
-        assert_eq!(input, vec![3, 4, 3, 1, 2]);
+        assert_eq!(input, vec![0, 1, 1, 2, 1, 0, 0, 0, 0]);
     }
 
     #[test]
     fn test_step() {
-        let mut fish = parse("inputs/day6_test").unwrap();
-        n_step(&mut fish, 1);
-        assert_eq!(fish, vec![2, 3, 2, 0, 1]);
+        let mut input = parse("inputs/day6_test").unwrap();
+        step(&mut input);
+        assert_eq!(input, vec![1, 1, 2, 1, 0, 0, 0, 0, 0]);
+        step(&mut input);
+        assert_eq!(input, vec![1, 2, 1, 0, 0, 0, 1, 0, 1]);
     }
 
     #[test]
     fn test_part_one() {
         let mut fish = parse("inputs/day6_test").unwrap();
-        n_step(&mut fish, 18);
-        assert_eq!(
-            fish,
-            vec![6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8]
-        );
-        n_step(&mut fish, 62);
-        assert_eq!(fish.len(), 5934);
+        n_step(&mut fish, 80);
+        assert_eq!(fish.iter().sum::<usize>(), 5934);
     }
 
     #[test]
     fn test_part_two() {
         let mut fish = parse("inputs/day6_test").unwrap();
-        n_step(&mut fish, 10);
-        assert_eq!(fish.len(), 26984457539);
+        n_step(&mut fish, 256);
+        assert_eq!(fish.iter().sum::<usize>(), 26984457539);
     }
 }
